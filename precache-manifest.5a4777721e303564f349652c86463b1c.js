e94fb7fce60d46b3c00895e5849845950d6e37cd
Elena P <elena.pan.ep@gmail.com>
2021-01-09 20:50:42 -0700

    Updates

---
+++ precache-manifest.5a4777721e303564f349652c86463b1c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b3d5e39786e0e0dc66d514c54198d99",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5e60eb1457d7697de058",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "4e16e76b200913da4bef",
+    "url": "/static/js/2.dc3a1b63.chunk.js"
+  },
+  {
+    "revision": "55449cd8a9ff6f64431b36dead82c80d",
+    "url": "/static/js/2.dc3a1b63.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5e60eb1457d7697de058",
+    "url": "/static/js/main.225593b7.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "a2b61295d5cd02cfc551dd2534f61ce4",
+    "url": "/static/media/galaxy.a2b61295.jpeg"
+  },
+  {
+    "revision": "563dba5c16a261d578318928c3638370",
+    "url": "/static/media/monopoly-demo.563dba5c.gif"
+  }
+]);
